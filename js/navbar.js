0b105cd75ca68d7b3b62380bb4e11bce508d6eaa
ssupix <151896598+ssupix@users.noreply.github.com>
2024-04-05 12:18:37 -0700

    navigation color fixed

---
+++ js/navbar.js
@@ -0,0 +1,16 @@
+gsap.registerPlugin(ScrollTrigger);
+const sections = gsap.utils.toArray(".section-dark");
+sections.forEach((section) => {
+  ScrollTrigger.create({
+    trigger: section,
+    duration: 3,
+    start: "top 30px",
+    end: "bottom 50px",
+    scrub: 1,
+    toggleClass: {
+      targets: ".nav-link",
+      className: "text-white"
+    },
+    // markers: true
+  });
+});
