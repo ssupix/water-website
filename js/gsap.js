eaa5bd1c00d6b9ff7f304990c3cf86f0b46d203d
ssupix <151896598+ssupix@users.noreply.github.com>
2024-04-04 20:13:27 -0700

    scroll trigger test

--- js/gsap.js
+++ js/gsap.js
@@ -22,3 +22,13 @@ gsap.to(".running ul li", {
     repeat: -1
 });
 
+const contents = gsap.utils.toArray("#river .content");
+
+gsap.to(contents, {
+    xPercent: -100 * (contents.length -1),
+    // scrollTrigger: {
+    //     trigger: "#river",
+    //     pin: true,
+    //     scrub: 1
+    // }
+})
