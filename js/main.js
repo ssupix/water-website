05320117a85106807664de36f502eeb5485b2eae
ssupix <151896598+ssupix@users.noreply.github.com>
2024-04-08 20:27:15 -0700

    started working on scroll reveal

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
-
 $(function(){
-
+    // Parallax initialization
     $(".sea-shore-parallax").parallax({
-
         imageSrc: "assets/imgs/sea.jpg"
-
     });
 
     $(".about-img").parallax({
-
         imageSrc: "assets/imgs/this.jpeg"
+    });
 
+    $(".lake-image").parallax({
+        imageSrc: "assets/imgs/lake.png"
     });
 
     $(window).trigger('resize').trigger('scroll');
 
     $(".my-background-video").bgVideo({
-
         pauseAfter: 0,
         fadeIn: 0,
         showPausePlay: false
-        
     });
 
-
+    // ScrollReveal initialization
+    ScrollReveal().reveal('.fish', {
+         delay: 300, 
+         easing: 'ease-in-out' });
 });
 
